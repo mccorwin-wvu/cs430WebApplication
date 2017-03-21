2ae53b7e7ba98eba454df0b34a9aab3df89e1c7c
mccorwin <mccorwin@mix.wvu.edu>
2017-03-20 20:15:26 -0400

    Add project files.

---
+++ cs430WebApplication/Login.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="cs430WebApplication.WebForm1" %>
+
+<!DOCTYPE html5>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    
+    <link href="css/bootstrap.css" rel="stylesheet" />
+    <title></title>
+
+    <style>
+body {
+    background-image: url(http://wallpapercave.com/wp/0gjtD62.jpg);
+}
+</style>
+   
+    
+</head>
+<body>
+
+ 
+
+    <div class="container">
+
+<div class="row" style="margin-top:20px">
+    <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+		<form role="form">
+			<fieldset>
+				<h2 class = "text-center" style="color: #ffed00">Please Sign In </h2>
+				<hr class="colorgraph">
+				<div class="form-group">
+                    <input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address">
+				</div>
+				<div class="form-group">
+                    <input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password">
+				</div>
+				<span class="button-checkbox" style="color:#ffd800">
+                    <input type="checkbox" name="remember_me" id="remember_me" checked="checked"> Remember Me
+					<a href="" class="btn btn-link pull-right" style="color:#ffd800">Forgot Password?</a>
+				</span>
+				<hr class="colorgraph">
+				<div class="row">
+					<div class="col-xs-6 col-sm-6 col-md-6">
+                        <input type="submit" class="btn btn-lg btn-success btn-block" value="Sign In">
+					</div>
+					<div class="col-xs-6 col-sm-6 col-md-6">
+						<a href="Register.aspx" class="btn btn-lg btn-primary btn-block">Register</a>
+					</div>
+				</div>
+			</fieldset>
+		</form>
+	</div>
+</div>
+
+</div>
+
+
+
+
+</body> 
+</html>
---
+++ cs430WebApplication/Register.aspx.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+
+namespace cs430WebApplication
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            serverCheck();
+
+            
+
+           
+
+
+        }
+
+       
+       private String server;
+       private String database;
+       private String uid;
+       private String password;
+        private MySqlConnection conn;
+
+        //Response.Write("button clicked");
+        // Register(firstName.Value,lastName.Value,email.Value,confirmPassword.Value,tagBox.Value,School.Value);
+        protected void Submit_Click(object sender, EventArgs e)
+
+        {
+
+            Register(firstName.Value, lastName.Value, email.Value, confirmPassword.Value, tagBox.Value, School.Value);
+            Response.Redirect("Login.aspx");
+
+        }
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+        public void serverCheck()
+
+
+        {
+
+            server = "us-cdbr-azure-east2-d.cloudapp.net";
+            database = "cs430dp";
+            uid = "bde01a074310a7";
+            password = "2316fdf1";
+
+            string connString;
+            connString = $"SERVER={server};DATABASE={database};UID={uid};PASSWORD={password};";
+            conn = new MySqlConnection(connString);
+            using (conn)
+            {
+                conn.Open();
+                Response.Write("connected");
+            }
+
+        }
+
+
+
+        public bool Register(string firstName, string lastName, string email, string password, string tags, string school)
+        {
+            string query = $"INSERT INTO users ( email, first_name, last_name, password, tags, school) VALUES ('{email}','{firstName}','{lastName}','{password}','{tags}','{school}');";
+            Response.Write("registering");
+
+            try
+            {
+                if (OpenConnection())
+                {
+                    MySqlCommand cmd = new MySqlCommand(query,conn);
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                        return true;
+                    }
+                    catch(Exception ex)
+                    {
+                        return false;
+                    }
+                }
+
+                else
+                {
+                    conn.Close();
+                    return false;
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                return false;
+            }
+
+        }
+
+        public bool IsLogin(string email, string pass)
+        {
+
+
+
+            string query = $"SELECT * users FROM useres WHERE email= '{email}' AND password= '{pass}';";
+
+
+            try
+            {
+                if (OpenConnection())
+                {
+
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        reader.Close();
+                        conn.Close();
+                        return true;
+
+                    }
+                    else
+                    {
+                        reader.Close();
+                        conn.Close();
+                        return false;
+                    }
+                }
+                else
+                {
+                    conn.Close();
+                    return false;
+                }
+            }
+
+            catch(Exception ex) {
+
+
+                conn.Close();
+                return false;
+
+            }
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                conn.Open();
+                return true;
+
+
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert connection to server failed!", true);
+                        break;
+                    case 1045:
+                        ClientScript.RegisterStartupScript(this.GetType(), "myalert", "Server Username or password is incorrect!", true);
+                        break;
+
+                }
+                return false;
+            }
+        }
+
+        
+    }
+}
